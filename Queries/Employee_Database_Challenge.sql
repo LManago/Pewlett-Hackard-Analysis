087a2988fd9e6a1cbdfcb8557d3af2e67a6b66d5
LManago <71472813+LManago@users.noreply.github.com>
2020-11-08 22:03:56 -0500

    Updating Challeneg code

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -36,3 +36,24 @@ GROUP BY title
 ORDER BY COUNT(title) DESC;
 
 SELECT * FROM retiring_titles;
+
+--DELIVERABLE 2
+SELECT * FROM dept_emp;
+
+SELECT DISTINCT ON(e.emp_no)e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+tit.title
+--DISTINCT ON(emp_no)
+--INTO mentor_eligibility
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS tit
+ON (e.emp_no = tit.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY emp_no;
